f5bea620cbdd84ff17019931b5e562a7d008f43e
Kishanukani <ukanikishanuk@gmail.com>
2024-02-04 13:27:20 +0530

    upload not working in my page,working on seperate page

--- PP/Resume/urls.py
+++ PP/Resume/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("", views.welcome),
-    path("resume/", views.Resume, name="resume"),
+    path("resumepage/", views.resumepage, name="resumepage"),
+    path("index", views.index, name="index"),
     path("sign_up/", views.Signup, name="sign_up"),
-]
+    path(
+        "upload_resume/", views.upload_resume, name="upload_resume"
+    ),  # Use a different URL pattern for upload_resume
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- PP/Resume/views.py
+++ PP/Resume/views.py
@@ -1,17 +1,51 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.conf import settings
+import firebase_admin
+from firebase_admin import credentials, storage
+from datetime import timedelta
 
 
 def welcome(request):
     return render(request, "index.html")
 
 
-def Resume(request):
+def index(request):
+    return render(request, "index.html")
+
+
+def resumepage(request):
     return render(request, "Resume.html")
 
 
 def Signup(request):
     return render(request, "sign_up.html")
+
+
+def upload_resume(request):
+    if request.method == "POST" and request.FILES["uploadresume"]:
+        upload_resume = request.FILES["uploadresume"]
+
+        # Initialize Firebase Admin SDK
+        cred = credentials.Certificate(settings.FIREBASE_SERVICE_ACCOUNT_KEY_PATH)
+        # firebase_admin.initialize_app(
+        #     cred,
+        #     name="ResumeApp",
+        #     options={"storageBucket": settings.FIREBASE_STORAGE_BUCKET},
+        # )
+        bucket = storage.bucket(settings.FIREBASE_STORAGE_BUCKET)
+
+        # Store user's resume in Firebase Storage
+        filename = upload_resume.name
+        blob = bucket.blob(f"userresume/{filename}")
+        blob.upload_from_file(upload_resume)
+
+        context = {"resume_uploaded": True}
+    else:
+        context = {"resume_uploaded": False}
+
+    return render(request, "upload_resume.html", context)
 
 
 # Create your views here.
