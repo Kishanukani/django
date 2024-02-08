7d8ba7dbc0646e7d99be521376f13b98b7d3701b
Kishanukani <ukanikishanuk@gmail.com>
2024-02-08 20:50:07 +0530

    resume analysis done

--- PP/PP/urls.py
+++ PP/PP/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     # path("",include())
     path("", include("Resume.urls")),
+    path("", include("resumeanalysis.urls")),
     path("admin/", admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
